b60aa8920c6a6e51913f87938330b51bd202b67f
Laz627 <169095963+Laz627@users.noreply.github.com>
2025-03-28 16:54:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1777,7 +1777,7 @@ def generate_internal_links_with_embeddings(article_content: str, pages_with_emb
         try:
             # Get paragraph embeddings
             response = openai.Embedding.create(
-                model="text-embedding-3-small",
+                model="text-embedding-3-large",
                 input=paragraph_texts
             )
             paragraph_embeddings = [item['embedding'] for item in response['data']]
